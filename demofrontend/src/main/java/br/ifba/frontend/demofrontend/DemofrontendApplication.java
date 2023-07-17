ad64f224a29e7626824db2f9d5fc7e591e0db6bf
Mur1love <murilormleal@outlook.com>
2023-07-17 15:30:20 -0300

    commit do projeto com springboot

---
+++ demofrontend/src/main/java/br/ifba/frontend/demofrontend/DemofrontendApplication.java
@@ -0,0 +1,13 @@
+package br.ifba.frontend.demofrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofrontendApplication.class, args);
+	}
+
+}
---
+++ demofrontend/src/test/java/br/ifba/frontend/demofrontend/DemofrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifba.frontend.demofrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
