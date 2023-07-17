c276139cde6b23308388221f4617633c02aeecd1
Mur1love <murilormleal@outlook.com>
2023-07-17 15:42:16 -0300

    controller e confi de porta de server

---
+++ frontend/src/main/java/br/ifba/demo/frontend/controller/HomeController.java
@@ -0,0 +1,29 @@
+package br.ifba.demo.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController{
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
+	}
+
+    @GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/signup")
+	public String signup() {
+		return "signup";
+	}
+
+}
