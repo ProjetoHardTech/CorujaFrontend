ce0afaf62362c797890c88a2788d083d051995d7
bglassesgirl <luanabragagomes9@gmail.com>
2023-08-02 10:20:08 -0300

    botoes e logo concluida

--- frontend/src/main/java/br/ifba/demo/frontend/controller/HomeController.java
+++ frontend/src/main/java/br/ifba/demo/frontend/controller/HomeController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController{
 
-    @GetMapping
+    @GetMapping("/index")
     public String index() {
         return "index";
     }
-
+	 @GetMapping
+    public String index() {
+        return "index";
+    }
     @GetMapping("/dashboard")
 	public String dashboard() {
 		return "dashboard";
