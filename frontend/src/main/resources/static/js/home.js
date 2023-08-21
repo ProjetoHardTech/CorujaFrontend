ae4e30468dcaf6cfd42bd66567fc79418e9d8006
Luana Braga <luanabragagomes9@gmail.com>
2023-08-20 22:19:37 -0300

    correcao de bugs e att no js

--- frontend/src/main/resources/static/js/home.js
+++ frontend/src/main/resources/static/js/home.js
@@ -1,6 +1,10 @@
 //side-bar
 const menuItems = document.querySelectorAll('.menu-item');
 
+//theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+
 //remover as classes ativas de todas os menus
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -12,14 +16,25 @@ menuItems.forEach(item => {
     item.addEventListener('click', () => {
         changeActiveItem();
         item.classList.add('active');
-        if(item.id != 'notificatons'){
-            document.querySelector('.notification-popup').
-            style.display = 'none';
-        }else{
-            document.querySelector('.notification-popup').
-            style.display = 'block';
-            document.querySelector('#notifications . notification-count').
-            style.display = 'none';
+        
+        if (item.id !== 'notifications') {
+            document.querySelector('.notification-popup').style.display = 'none';
+        } else {
+            document.querySelector('.notification-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
         }
-    })
-})
+    });
+});
+
+const openThemeModal = () =>{
+    themeModal.style.display = 'grid';
+}
+const closeThemeModal = (e) =>{
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
