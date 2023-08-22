69c39877cd74b36671975aebc080d605bbc3ebc9
Luana Braga <luanabragagomes9@gmail.com>
2023-08-22 13:50:45 -0300

    melhorando js

--- frontend/src/main/resources/static/js/home.js
+++ frontend/src/main/resources/static/js/home.js
@@ -4,6 +4,8 @@ const menuItems = document.querySelectorAll('.menu-item');
 //theme
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
 
 //remover as classes ativas de todas os menus
 const changeActiveItem = () => {
@@ -38,3 +40,36 @@ const closeThemeModal = (e) =>{
 themeModal.addEventListener('click', closeThemeModal);
 
 theme.addEventListener('click', openThemeModal);
+
+fontSizes.forEach(size => {
+    let fontSize;
+
+    size.addEventListener('click', () => {
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('--stick-top-left', '5.4rem');
+            root.style.setProperty('--stick-top-right', '5.4rem');
+        } else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('--stick-top-left', '5.4rem');
+            root.style.setProperty('--stick-top-right', '-7rem');
+        }else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('--stick-top-left', '-2rem');
+            root.style.setProperty('--stick-top-right', '-17rem');
+        } else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('--stick-top-left', '-5rem');
+            root.style.setProperty('--stick-top-right', '-25rem');
+        } else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('--stick-top-left', '-12rem');
+            root.style.setProperty('--stick-top-right', '-35rem');
+        }
+        document.querySelector('html').style.fontSize = fontSize;
+
+    });
+
+
+
+})
